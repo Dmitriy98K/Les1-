c5039a77b128a87f3b8f22b0ec924b1fde123aa7
Dmitriy98K <kolesnyy98@mail.ru>
2022-12-16 16:34:50 +0300

    любимчик, ливитация

---
+++ masha/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите имя пользователя ");
+string username = Console.ReadLine();
+// ToLower() ливитирует маШа МашА
+if(username.ToLower()=="маша")
+{
+    Console.WriteLine("ES!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+
+}
