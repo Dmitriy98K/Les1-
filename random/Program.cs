ef0d672e63dc0625dfe7316f4894e76f64980c91
Dmitriy98K <kolesnyy98@mail.ru>
2022-12-16 16:14:07 +0300

    рандом

---
+++ random/Program.cs
@@ -0,0 +1,9 @@
+﻿//Что нового
+//Случайности
+//new Random().Next(min, max)
+//даст случайное целое число от min до max-1
+//[min, max-1] или [min, max)
+int numberA = new Random().Next(1,10);//1,,,9
+int numberB = new Random().Next(1,10);
+int result =numberA+numberB ;
+Console.WriteLine(result);
