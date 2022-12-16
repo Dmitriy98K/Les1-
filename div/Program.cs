4170ff5a3edbc241c7d081de1905d07420b712bf
Dmitriy98K <kolesnyy98@mail.ru>
2022-12-16 16:05:23 +0300

    деление , типы данных

---
+++ div/Program.cs
@@ -0,0 +1,10 @@
+﻿int numberA = 10;
+int numberB =5;
+Console.WriteLine(numberA/numberB);
+double numberC = 3;
+double numberD =5;
+Console.WriteLine(numberC/numberD);
+//int От -2 147 483 648 до 2 147 483 647 32-разрядное целое число со
+//ouble от ±5,0 × 10−324 до ±1,7 × 10308 64-разрядное целое число со
+//string от 0 до много символов от 4 байт до 2 гб
+//bool true и false 1 байт
